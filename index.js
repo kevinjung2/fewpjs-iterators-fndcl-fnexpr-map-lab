d90569e48a2e8e05bc7ae6017e57cd5726871647
Kevin Jung <kev.jung.ii@gmail.com>
2021-04-26 21:48:40 -0400

    commit tree 4b56963143e6bc20c351abfa6dbafbf0bf9b03b7

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => title.split(" ")).map(title => title.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" "))
 }
